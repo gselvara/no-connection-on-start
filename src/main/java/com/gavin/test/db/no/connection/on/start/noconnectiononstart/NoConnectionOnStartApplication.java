3dae96a45590100aa0571a4c2b1fb4fdd6dc7bde
Gavin Selvaratnam <gselvaratnam@caiso.com>
2019-01-16 13:55:41 -0800

    Initial commit

---
+++ src/main/java/com/gavin/test/db/no/connection/on/start/noconnectiononstart/NoConnectionOnStartApplication.java
@@ -0,0 +1,14 @@
+package com.gavin.test.db.no.connection.on.start.noconnectiononstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoConnectionOnStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoConnectionOnStartApplication.class, args);
+	}
+
+}
+
